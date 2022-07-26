71392b04f157d170852d1ca7226675f031d44b15
Fernando <Fernando@DESKTOP-NFDMETJ>
2022-07-25 19:47:52 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace semana2_primer_proyecto_console_calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
