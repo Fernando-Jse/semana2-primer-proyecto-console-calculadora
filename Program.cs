6388e5589c61d4146fc318e0a498e484d0795d7e
Fernando José López Torres <USTS002422@ugb.edu.sv>
2022-07-29 14:46:10 -0600

    Trabaje en el codigo del proyecto de consola, agregando el otro class en donde ya esta editado el class tanto de calculator como de program

--- Program.cs
+++ Program.cs
@@ -5,53 +5,109 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace semana2_primer_proyecto_console_calculadora
-{ 
+{
+    class calculadora
+    {
+        public static double DoOperation(double num1, double num2, string op)
+        {
+            double result = double.NaN; 
+
+            switch (op)
+            {
+                case "a":
+                    result = num1 + num2;
+                    break;
+                case "s":
+                    result = num1 - num2;
+                    break;
+                case "m":
+                    result = num1 * num2;
+                    break;
+                case "d":
+                    
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    break;
+                
+                default:
+                    break;
+            }
+            return result;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            bool endApp = false;
             
-            int num1 = 0; int num2 = 0;
-
-            
-            Console.WriteLine("Calculadora en Consola de C#\r");
+            Console.WriteLine("Calculadora de consola en C#\r");
             Console.WriteLine("------------------------\n");
 
-            
-            Console.WriteLine("Digite un número, y presione ENTER para continuar");
-            num1 = Convert.ToInt32(Console.ReadLine());
-
-            
-            Console.WriteLine("Digite otro número, Y presione ENTER para continuar");
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-           
-            Console.WriteLine("Elija una opción de la lista que se muestra a contiuación:");
-            Console.WriteLine("\ta - Sumar");
-            Console.WriteLine("\ts - Restar");
-            Console.WriteLine("\tm - Multiplicar");
-            Console.WriteLine("\td - Dividir");
-            Console.Write("Tú Opción? ");
-
-            
-            switch (Console.ReadLine())
+            while (!endApp)
             {
-                case "a":
-                    Console.WriteLine($"Tu Resultado: {num1} + {num2} = " + (num1 + num2));
-                    break;
-                case "s":
-                    Console.WriteLine($"Tu Resultado: {num1} - {num2} = " + (num1 - num2));
-                    break;
-                case "m":
-                    Console.WriteLine($"Tu Resultado: {num1} * {num2} = " + (num1 * num2));
-                    break;
-                case "d":
-                    Console.WriteLine($"Tu Resultado: {num1} / {num2} = " + (num1 / num2));
-                    break;
+                
+                string numInput1 = "";
+                string numInput2 = "";
+                double result = 0;
+
+                
+                Console.Write("Digite un número, y presione ENTER: ");
+                numInput1 = Console.ReadLine();
+
+                double cleanNum1 = 0;
+                while (!double.TryParse(numInput1, out cleanNum1))
+                {
+                    Console.Write("Esta entrada no es valida. Por favor ingrese un valor entero: ");
+                    numInput1 = Console.ReadLine();
+                }
+
+                
+                Console.Write("Escriba otro número, Y luego presione ENTER: ");
+                numInput2 = Console.ReadLine();
+
+                double cleanNum2 = 0;
+                while (!double.TryParse(numInput2, out cleanNum2))
+                {
+                    Console.Write("Esta entrada no es valida. Por favor ingrese un valor entero: ");
+                    numInput2 = Console.ReadLine();
+                }
+
+                
+                Console.WriteLine("Elija un operador de la siguiente lista:");
+                Console.WriteLine("\ta - Suma");
+                Console.WriteLine("\ts - Resta");
+                Console.WriteLine("\tm - Multiplicación");
+                Console.WriteLine("\td - Divición");
+                Console.Write("Tú opción? ");
+
+                string op = Console.ReadLine();
+
+                try
+                {
+                    result = calculadora.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("Está operación dara como resultado un error matemático.\n");
+                    }
+                    else Console.WriteLine("Tú resultado: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! Ocurrió una excepción tratando de hacer los cálculos.\n - Detalles: " + e.Message);
+                }
+
+                Console.WriteLine("------------------------\n");
+
+                
+                Console.Write("Presione 'n' y Enter para cerrar la app, o presione cualquier otra tecla y Enter para continuar: ");
+                if (Console.ReadLine() == "n") endApp = true;
+
+                Console.WriteLine("\n"); 
             }
-            
-            Console.Write("Presione cualquier tecla para cerrar la aplicación de la consola Calculadora...");
-            Console.ReadKey();
+            return;
         }
     }
 }
